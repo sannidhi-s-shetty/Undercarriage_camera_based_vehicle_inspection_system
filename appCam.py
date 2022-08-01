dbe388b1c8c1918763cfc8401d5e4361328096ff
sannidhi-s-shetty <sannidhishetty30@gmail.com>
2022-08-01 21:05:57 +0530

    Added code wrt laptop camera

--- appCam.py
+++ appCam.py
@@ -1,14 +1,19 @@
 # connect camera->Raspberry Pi Configuration Tool ->Camera Interface ->enabled->reboot
 # install Flask on Rpi : sudo apt-get install python3-flask
 
-from flask import Flask,render_template,Response
+from flask import Flask, render_template, Response
+
 from camera_pi import Camera
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     """" video streaming home page """
     return render_template("index.html")
+
+
 def gen(camera):
     """" video streaming generator function """
     while True:
@@ -16,13 +21,13 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
-        """Video streaming route. Put this in the src attribute of an img tag."""
-        return Response(gen(Camera()),
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
-
-
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
